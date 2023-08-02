fc4ca847aed8df72f1af4fca0e075e4034edcc72
derdeche <foued.derdeche@gmail.com>
2023-08-02 10:14:49 +0200

    add case realisateur,genre ,role

--- index.php
+++ index.php
@@ -13,10 +13,15 @@ if(isset($_GET["action"])){
     {
         case"listFilms" : $ctrlCinema->listFilms();break;
         case"listActeurs" : $ctrlCinema->listActeurs();break;
+        case"listRealisateurs" :$ctrlCinema->listRealisateurs();break;
+        case "listGenres" :$ctrlCinema->listGenres();break;
+        case"listRoles" :$ctrlCinema->listRoles();break;
     }
 }
 else{
     $ctrlCinema->listFilms();
     $ctrlCinema->listActeurs();
-
+    $ctrlCinema->listRealisateurs();
+    $ctrlCinema->listGenres();
+    $ctrlCinema->listRoles();
 };
