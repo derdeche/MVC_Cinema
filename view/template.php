a53834229bf6fa53901a495136ce9239c7441867
derdeche <foued.derdeche@gmail.com>
2023-08-18 22:53:20 +0200

    modif nav

--- view/template.php
+++ view/template.php
@@ -14,13 +14,13 @@
                 <h1><strong>AllMovies</strong></h1>
                     <nav>
                         <ul>
-                            <li><button class="button1"><strong>Films</strong></button></li>
-                            <li><button class="button1"><strong>Acteurs</strong></button></li>
-                            <li><button class="button1"><strong>Réalisateurs</strong></button></li>
-                            <li><button class="button1"><strong>Genres</strong></button></li>
-                            <li><button class="button1"><strong>Rôles</strong></button></li>
-                            <li><button class="button1"><strong>Castings</strong></button></li>
-                            <li><button class="button1"><strong>Ajout</strong></button></li>
+                            <li><a href="index.php?action=listFilms"><button class="button1"><strong>Films</strong></button></a></li>
+                            <li><a href="index.php?action=listActeurs"><button class="button1"><strong>Acteurs</strong></button></a></li>
+                            <li><a href="index.php?action=listRéalisateurs"><button class="button1"><strong>Réalisateurs</strong></button></a></li>
+                            <li><a href="index.php?action=listGenres"><button class="button1"><strong>Genres</strong></button></a></li>
+                            <li><a href="index.php?action=listRôles"><button class="button1"><strong>Rôles</strong></button></a></li>
+                            <li><a href="index.php?action=listCastings"><button class="button1"><strong>Castings</strong></button></a></li>
+                            <li><a href="index.php?action=listAjout"><button class="button1"><strong>Ajout</strong></button></a></li>
                         </ul>
                     </nav>
                     
