5107496f747a8ecea58b3832ca923aab3a6bb099
derdeche <foued.derdeche@gmail.com>
2023-08-18 16:07:18 +0200

    add header

--- view/template.php
+++ view/template.php
@@ -3,13 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="public/style.css">
     <title>Document</title>
 </head>
-<body>
-    <div id="wrapper">
-        <?= $content?>
-        
-    </div>
+<body class ='fond'>
+    <!-- header -->
+    
+<div class='header' >
+                <h1><strong>AllMovies</strong></h1>
+                    <nav>
+                        <ul>
+                            <li><button class="button1"><strong>Films</strong></button></li>
+                            <li><button class="button1"><strong>Acteurs</strong></button></li>
+                            <li><button class="button1"><strong>Réalisateurs</strong></button></li>
+                            <li><button class="button1"><strong>Genres</strong></button></li>
+                            <li><button class="button1"><strong>Rôles</strong></button></li>
+                            <li><button class="button1"><strong>Castings</strong></button></li>
+                            <li><button class="button1"><strong>Ajout</strong></button></li>
+                        </ul>
+                    </nav>
+                    
+</div>
+
+<?= $content?>
 </body>
 </html>
