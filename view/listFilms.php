543cf90d2f517f853edf678008122ad30caec99c
derdeche <foued.derdeche@gmail.com>
2023-08-18 22:52:08 +0200

    modif class

--- view/listFilms.php
+++ view/listFilms.php
@@ -1,8 +1,5 @@
 <?php ob_start();?>
-<p class="uk-label uk-label-warning">Il y a 
-    <?= $requete->rowCount() ?> films</p>
 
-<table class = "uk-table uk-table-striped">
     <thead>
         <tr>
             <th>TITRE</th>
