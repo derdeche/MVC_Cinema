afd048fafef47a782cc5a34a9e2e18702f189a83
derdeche <foued.derdeche@gmail.com>
2023-08-21 02:33:44 +0200

    add formulaire ajoutActeur

---
+++ view/ajoutActeur.php
@@ -0,0 +1,23 @@
+<?php
+ob_start();
+?>
+<form action="index.php?action=ajoutActeur" method="post">
+
+    <label >Prénom :</label>
+    <input type="textarea" name="prenom" id="prenom">
+
+    <label >Nom :</label>
+    <input type="textarea" name="nom" id="nom">
+   
+    <label >Date de Naissance :</label>
+    <input type="date" name="dateNaissance" id="dateNaissance">
+
+    <input type="submit" name="submit" value="Ajouter l'acteur'">
+</form>
+
+<?php
+$title = "Ajouter un acteur";
+$secondary_title = "Ajouter un Acteur ";
+$content = ob_get_clean();
+require "view/Ajout.php"
+?>
