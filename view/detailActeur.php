cc2b904f2c5beb832e70ac396c40cb75d9f6fa55
derdeche <foued.derdeche@gmail.com>
2023-08-21 02:37:08 +0200

    affichage detail acteur

--- view/detailActeur.php
+++ view/detailActeur.php
@@ -1,10 +1,22 @@
 <?php
 ob_start();
-$acteur = $requeteA->fetch();
-echo $acteur["nom"] ."<br>";
-echo   $acteur["prenom"] ;
-
-
+<?php
+    foreach ($requete->fetchAll() as $acteur) { ?>
+        <div class="card-film">
+            <div class="container-flex">
+                    <div class="photo-acteur">
+                    <a href="index.php?action=detailsActeur&id=<?= $acteur["id_acteur"] ?>">
+                    <img src="<?= $actor["photo"] ?>" >
+                    </a>
+                </div>
+                <div class="infos-acteur">
+                    <p><?= $actor["nom"] . " " . $actor["prenom"] ?></p>
+                    <p><?= $actor["dateNaissance"] ?></p>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+<?php
 
 
 
