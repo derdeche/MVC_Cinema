b8fe2e24dcd1e2a1e63e3819de681355b2a1bd0a
derdeche <foued.derdeche@gmail.com>
2023-08-21 02:38:21 +0200

    affichage detail realisateur

--- view/detailRealisateur.php
+++ view/detailRealisateur.php
@@ -1,15 +1,18 @@
 <?php
-ob_start();
-$realisateur = $requeteR->fetch();
-echo $realisateur["nom"];
-
-
-
-
-
-
-
-
+ob_start();?>
+<p class= "titre">Liste des Réalisateurs</p>
+<?php
+    foreach ($requete->fetchAll() as $acteur) { ?>
+                    
+                <div class="photo-acteur">
+                    <a href="index.php?action=detailsActeur&id=<?= $acteur["id_acteur"] ?>">
+                    <img src="<?= $acteur["photo"] ?>" ></a>
+                </div>
+                
+        <?php } ?>
+<?php
+//$titre = "Liste des Réalisateurs";
+//$titre_secondaire = "Liste des Réalisateurs";
 $content = ob_get_clean();
 
 require "view/template.php";
