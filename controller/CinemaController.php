18554f6f38d1da006387c0369a5a4ba5bc82ce63
derdeche <foued.derdeche@gmail.com>
2023-08-21 02:32:23 +0200

    add function listCastings, ajoutFilm

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -17,115 +17,143 @@ class CinemaController
 
     public function listActeurs(){
         $pdo = Connect::seConnecter();
-        $requeteA = $pdo->query("
-        SELECT nom, prenom FROM personne
-        INNER JOIN acteur ON personne.id_personne = acteur.id_personne
-        ORDER BY id_acteur
+        $requete = $pdo->query("     
+        SELECT  nom, prenom, DATE_FORMAT(dateNaissance, '%d-%m-%Y') AS date, photo
+        FROM personne
+        INNER JOIN acteur ON personne.id_personne = acteur.id_personne               
         ");
         require "view/listActeurs.php";}
 
     public function listRealisateurs(){
         $pdo = Connect::seConnecter();
-        $requeteR = $pdo->query("
-        SELECT nom, prenom FROM personne
+        $requete = $pdo->query("
+        SELECT nom, prenom, DATE_FORMAT(dateNaissance, '%d-%m-%Y') AS date, photo FROM personne
         INNER JOIN realisateur ON personne.id_personne = realisateur.id_personne
-        ORDER BY id_realisateur
+        
         ");
         require "view/listRealisateurs.php";}
 
     public function listGenres(){
         $pdo = Connect::seConnecter();
-        $requeteG = $pdo->query("
+        $requete = $pdo->query("
         SELECT genre FROM genre
         ");
         require "view/listGenres.php";}
 
     public function listRoles(){
         $pdo = Connect::seConnecter();
-        $requeteR = $pdo->query("
+        $requete = $pdo->query("
         SELECT role FROM role
         ");
         require "view/listRoles.php";}
 
+    public function listCastings()
+    {
+        $pdo = Connect::seConnecter();
+        $request = $pdo->query("
+        SELECT nom, prenom, role, titre FROM jouer
+        INNER JOIN personne ON jouer.id_acteur = personne.id_personne        
+        INNER JOIN role ON jouer.id_role = role.id_role
+        INNER JOIN film ON jouer.id_film = film.id_film
+        INNER JOIN acteur ON personne.id_personne = acteur.id_personne
+        "
+        );
+        
+        require "view/listCastings.php";
+    }
+
     public function detailFilm ($id){
         $pdo = Connect ::seConnecter ();
-        $requeteF = $pdo->prepare("select titre from film where id_film = :id");
-        $requeteF->execute(["id"=>$id]);
+        $requete = $pdo->prepare("select titre from film where id_film = :id");
+        $requete->execute(["id"=>$id]);
         require "view/detailFilm.php";}   
 
     public function detailActeur ($id){
         $pdo = Connect ::seConnecter ();
-        $requeteA = $pdo->prepare("SELECT nom, prenom FROM personne 
+        $requete = $pdo->prepare("SELECT nom, prenom FROM personne 
         INNER JOIN acteur ON personne.id_personne = acteur.id_personne
         WHERE id_acteur =  :id");
-        $requeteA->execute(["id"=>$id]);
+        $requete->execute(["id"=>$id]);
         require "view/detailActeur.php";} 
 
     public function detailRealisateur ($id){
         $pdo = Connect ::seConnecter ();
-        $requeteR = $pdo->prepare("SELECT nom, prenom FROM personne
+        $requete = $pdo->prepare("SELECT nom, prenom FROM personne
         INNER JOIN realisateur ON personne.id_personne = realisateur.id_personne        
         WHERE id_realisateur =  :id"); 
-        $requeteR->execute(["id"=>$id]);
+        $requete->execute(["id"=>$id]);
         require "view/detailRealisateur.php";} 
 
     public function detailRole ($id){
         $pdo = Connect ::seConnecter ();
-        $requeteRole = $pdo->prepare("SELECT titre FROM film
+        $requete = $pdo->prepare("SELECT titre FROM film
         INNER JOIN jouer ON film.id_film = jouer.id_film        
         WHERE id_role =  :id"); 
-        $requeteRole->execute(["id"=>$id]);
+        $requete->execute(["id"=>$id]);
         require "view/detailRole.php";} 
 
     public function detailGenre ($id){
         $pdo = Connect ::seConnecter ();
-        $requeteGenre = $pdo->prepare("SELECT titre FROM film
+        $requete = $pdo->prepare("SELECT titre FROM film
         INNER JOIN action ON film.id_film = action.id_film     
         WHERE id_genre =  :id"); 
-        $requeteGenre->execute(["id"=>$id]);
+        $requete->execute(["id"=>$id]);
         require "view/detailGenre.php";} 
 
-    /*public function ajoutFilm (){
-        if(isset($_post["submit"])){
-            $titre = "titre";
-            $anneeSortie = "anneeSortie";
-            $duree = "duree";
-            $synopsis = "synopsis";
-            $note = "affiche";}}*/
+    public function ajoutFilm (){
+        $pdo = Connect::seConnecter();
+		
+		$requete = $pdo->query("
+			SELECT genre FROM genre 
+			");
 
-            public function ajoutGenre(){
-            if(isset($_POST['submit'])){
+        $requete = $pdo->query("
+		SELECT nom, prenom	FROM personne
+        INNER JOIN realisateur ON personne.id_personne = realisateur.id_personne
+        GROUP BY id_realisateur
 
-                // var_dump($_POST);die;    
-                $pdo = connect::seConnecter(); 
-                $requeteAjoutGenre= $pdo->prepare("
-                INSERT INTO genre (genre)
-                VALUES (:nomGenre)"
-                );
-                $requeteAjoutGenre->execute(['nomGenre' => $_POST['nomGenre']]);
-                $newId = $pdo->lastInsertId();
-                header("Location:index.php?action=detailGenre&id=".$newId);
-                die;
-            }
-                require "view/ajoutGenre.php";
+		");
+        require "view/ajoutFilm.php";
+    }
+
+    public function ajoutGenre(){
+    if(isset($_POST['submit'])){
+
+        // var_dump($_POST);die;    
+        $pdo = connect::seConnecter(); 
+        $requete= $pdo->prepare("
+        INSERT INTO genre (genre)
+        VALUES (:nomGenre)"
+        );
+        $requete->execute(['nomGenre' => $_POST['nomGenre']]);
+        $newId = $pdo->lastInsertId();
+        header("Location:index.php?action=detailGenre&id=".$newId);
+        die;
+    }
+        require "view/ajoutGenre.php";
                            
+    }
+
+    public function ajoutRole (){
+        if(isset($_POST['submit'])){
+            $pdo = connect::seConnecter();
+            $requete= $pdo->prepare("
+            INSERT INTO role (role)
+            VALUES (:nomRole)"
+            );
+            $requete->execute(['nomRole' => $_POST['nomRole']]);
+            $newId = $pdo->lastInsertId();
+            header("Location:index.php?action=detailRole&id=".$newId);
+            die;
+
         }
+            require "view/ajoutRole.php";
+    
+    }       
 
-        public function ajoutRole (){
-            if(isset($_POST['submit'])){
-                $pdo = connect::seConnecter();
-                $requeteAjoutRole= $pdo->prepare("
-                INSERT INTO role (role)
-                VALUES (:nomRole)"
-                );
-                $requeteAjoutRole->execute(['nomRole' => $_POST['nomRole']]);
-                $newId = $pdo->lastInsertId();
-                header("Location:index.php?action=detailRole&id=".$newId);
-                die;
-
-            }
-                require "view/ajoutRole.php";
-        
-        }       
+    public function Ajout()
+    {
+        require "view/listAjout.php";
+    }
           
 }
