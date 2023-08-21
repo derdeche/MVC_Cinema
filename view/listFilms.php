0350ccc1ccf3d3bfc40fe2b020737a93d11179c7
derdeche <foued.derdeche@gmail.com>
2023-08-21 02:41:25 +0200

    modif link a href

--- view/listFilms.php
+++ view/listFilms.php
@@ -1,15 +1,16 @@
 <?php ob_start();?>
 
     
-        <div class="container"> 
+        
     
             <?php foreach( $requete->fetchAll() as $film ){ ?>
-                                        
-            <div class = "photo">
-            <a href="index.php?action=detailFilm&id=<?= $film["id_film"] ?>">
-                        <img src="<?= $film["affiche"] ?>"  >                     
-            </div></a>
-        </div>
+                
+                <div class = "photo">
+                <a href="index.php?action=detailFilm&id=<?= $film["id_film"] ?>">
+                        <img src="<?= $film["affiche"] ?>">
+                </a>                  
+                </div>
+        <!-- </div> -->
         <?php }            
 
 
