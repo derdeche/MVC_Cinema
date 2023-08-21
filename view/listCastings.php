f53d2627faa0d0ce93a6e3005bd1626058740995
derdeche <foued.derdeche@gmail.com>
2023-08-21 02:40:05 +0200

    affichage liste casting

--- view/listCastings.php
+++ view/listCastings.php
@@ -1,11 +1,28 @@
 <?php 
-ob_start();
+ob_start();?>
 
-
-
-
-
-
+<p class= "titre">Liste des Castings</p>
+<?php   foreach ($request->fetchAll() as $jouer) { ?>
+    <div class="casting">
+        <p>L'acteur <a href="index.php?action=detailActeur&id=<?=$jouer['id_acteur']?>"><?=$jouer["nom"] . " " . $jouer["prenom"] ?></a></p> 
+        <p>a joué le Role de :<a href="index.php?action=detailRole&id=<?=$jouer['id_role']?>"> <?= $jouer["role"] ?> </a>
+        <p>dans : <a href="index.php?action=detailFilm&id=<?=$jouer['id_film']?>"> <?= $jouer["titre"] ?> </a></p> 
+    </div>                    
+<?php } ?>                    
+                  
+<?=
+$title = "Liste des Castings";
+$secondary_title = "Liste des Castings";
 
 $content = ob_get_clean();
 require "view/template.php";
+                            
+                              
+                                  
+                                    
+
+                                    
+                                
+                 
+        
+
