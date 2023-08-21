e935cf8ee3870ce4ae466db4fc40b12746867ab4
derdeche <foued.derdeche@gmail.com>
2023-08-21 11:36:42 +0200

    add afichage nom et prenom acteur

--- view/listActeurs.php
+++ view/listActeurs.php
@@ -1,11 +1,13 @@
 <?php ob_start();?>
-<p class= "titre">Liste des Acteurs</p>
+<p class= "titre"><strong>Liste des Acteurs</strong></p>
 <?php
     foreach ($requete->fetchAll() as $acteur) { ?>
                     
                 <div class="photo-acteur">
                     <a href="index.php?action=detailsActeur&id=<?= $acteur["id_acteur"] ?>">
-                    <img src="<?= $acteur["photo"] ?>" ></a>
+                    <img src="<?= $acteur["photo"] ?>" >
+                    <p ><strong><?= $acteur["nom"]." ".$acteur["prenom"] ?></strong></p>
+                    </a> 
                 </div>
                 
         <?php } ?>
