18620561908ad56600bdafbe5f5cd5fb206bd038
derdeche <foued.derdeche@gmail.com>
2023-08-22 10:59:48 +0200

    formulaire ajout realisateur

---
+++ view/ajoutRealisateur.php
@@ -0,0 +1,29 @@
+<?php
+ob_start();
+?>
+<form action="index.php?action=ajoutRealisateur" method="post">
+
+    <label >Prénom :</label>
+    <input type="textarea" name="prenom" id="prenom">
+
+    <label >Nom :</label>
+    <input type="textarea" name="nom" id="nom">
+   
+    <label >Date de Naissance :</label>
+    <input type="date" name="dateNaissance" id="dateNaissance">
+
+    <label >Sexe</label>
+    <input type="text" name="sexe" id="sexe">
+
+    <label >Photo</label>
+    <input type="varchar" name="photo" id="photo" placeholder="URL">
+
+    <input type="submit" name="submit" value="Ajouter le Réalisateur">
+</form>
+
+<?php
+$title = "Ajouter un Réalisateur";
+$secondary_title = "Ajouter un Réalisateur ";
+$content = ob_get_clean();
+require "view/template.php"
+?>
