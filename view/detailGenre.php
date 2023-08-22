74ec94e0d6fdc6e5c1f5c76cafd99a4713514adf
derdeche <foued.derdeche@gmail.com>
2023-08-22 16:45:48 +0200

    ajout link

--- view/detailGenre.php
+++ view/detailGenre.php
@@ -4,11 +4,14 @@ ob_start();
 /*affichage liste des films du meme genre */
 foreach( $requete->fetchAll() as $film){ ?>
         
-    <tr>
-        <td><?= $film["titre"]?></td><br>
+    
+    
+        <a href="index.php?action=detailFilm&id=<?= $film["id_film"] ?>">
+                        <img src="<?= $film["affiche"] ?>"></a>
+        <?= $film["titre"]?>
         
         
-    </tr>
+    
 
 <?php } 
 
