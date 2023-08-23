9f666156c0ffdcfda300bc4388cc059bc9246329
derdeche <foued.derdeche@gmail.com>
2023-08-23 16:11:29 +0200

    modif affichage

--- view/detailFilm.php
+++ view/detailFilm.php
@@ -1,20 +1,25 @@
 <?php
-ob_start();
+ob_start();?>
 
 
-$film = $requete->fetch();
-
-?>
-<?= $film["titre"] ?>
+<?php
+    foreach ($requete->fetchAll() as $film) { ?>
+                    
+                    <?= $film["titre"] ?>
 <p>Sortie en <?= $film["anneeSortie"] ?>
-        
+<p>Durée : <?=$film["duree"]?></p>
+<p><?=$film["synopsis"]?></p>
+<img src="<?=$film["affiche"]?>">
+<p>Note : <?=$film["note"]?></p>
+                
+        <?php } ?>     
         
     
 
 
 
 
-
+<?=
 $content = ob_get_clean();
 
 require "view/template.php";
